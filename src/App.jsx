5a4adfddfa52df30eea419c35f9b8e369afe2d16
Aca Milosavljević <aca.milosavljevic.dev@gmail.com>
2026-04-29 00:37:42 +0200

    Gallery improved UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import { FilePond, registerPlugin } from 'react-filepond';
 
 import './App.css'
 import Upload from './components/Upload';
-import { Gallery } from './components/Gallery';
+import Gallery from './components/Gallery';
 
 function App() {
   const [count, setCount] = useState(0)
   const [isUploadVisible, setIsUploadVisible] = useState(true);
 
   return (
-    <div className='flex flex-col p-5'>
+    <div className='flex flex-col p-5 size-full bg-[#121212]'>
       <div className='flex flex-row gap-5 w-full justify-center'>
-        <p className={`${isUploadVisible && 'text-blue-500'}`} onClick={() => setIsUploadVisible(true)}>Upload</p>
-        <p className={`${!isUploadVisible && 'text-blue-500'}`} onClick={() => setIsUploadVisible(false)}>Gallery</p>
+        <p className={`${isUploadVisible ? 'text-blue-500' : 'text-white'} cursor-pointer`} onClick={() => setIsUploadVisible(true)}>Upload</p>
+        <p className={`${!isUploadVisible ? 'text-blue-500' : 'text-white'}  cursor-pointer`} onClick={() => setIsUploadVisible(false)}>Gallery</p>
       </div>
       {isUploadVisible ? <Upload /> : <Gallery />}
     </div>
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,23 +1,42 @@
 import { useEffect, useState } from "react"
+import { RowsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/rows.css";
 
-export function Gallery() {
-    const [items, setItems] = useState(null);
+export default function Gallery() {
+    const [items, setItems] = useState([]);
+    const [selectedImage, setSelectedImage] = useState(null);
 
     useEffect(() => {
         fetch("http://localhost:3001/images")
             .then(res => res.json())
             .then(data => {
-                console.log('data',data);
-                
-                setItems(data)});
+                const items = data
+                    .filter(item => item.type === "image")
+                    .map(item => ({
+                        src: item.url,
+                    }));
+
+                setItems(items)
+            });
     }, [])
 
-    return (<div className="gallery">
-        {items?.map((item, index) => (item.type === 'image' ? 
-            <img key={index} src={item.url} alt="" /> : <video
-          src={item.url}
-          controls
-          style={{ width: "100%", borderRadius: "10px" }}
-        />))}
-    </div>)
+    return (items &&
+        <div className="flex flex-col w-full justify-center items-center relative"><RowsPhotoAlbum
+            onClick={(image) => {
+                setSelectedImage(image.photo.src);
+            }}
+            photos={items}
+            spacing={20}
+            componentsProps={{ container: { style: { height: '100%', margin: 20, padding: 20, backgroundColor: '#121212', border: '2px', borderRadius: '8px' } }, image: { style: { padding: 10, background: '#FFFFFF', borderRadius: '8px' } } }}
+
+        />
+            {selectedImage && <div className="flex flex-col w-fit h-fit absolute bg-blue-500 p-3">
+                <div className="flex flex-col w-full h-full relative">
+                    <p className="right-4 top-2 cursor-pointer text-white absolute hover:font-bold" onClick={() => setSelectedImage(null)}>X</p>
+                    <img className="max-h-[400px] max-w-[300px]" src={selectedImage} alt="" />
+                </div>
+            </div>}
+        </div>
+
+    )
 }
